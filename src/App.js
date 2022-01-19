335cd6fae898772c1bd8879d263560cf515c923d
amcmullen <amcmullen9291@gmail.com>
2022-01-19 11:09:34 -0500

    added dependencies and Home.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Components/Home';
 
 function App() {
   return (
-    <div className="App">
-    </div>
-  );
+    <Router>
+      <Routes>
+          <Route path="/" element={<Home/>} />
+      </Routes>
+  </Router>
+);
 }
 
 export default App;
---
+++ src/Components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home(props) {
+    return (
+        <div className="mainBackground">
+           <center>Home Page</center> 
+        </div>
+    );
+}
+
+export default Home;
