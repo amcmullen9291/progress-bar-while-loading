9ffbe6dbba153b2626c621a7c01cd75accd968e8
amcmullen <amcmullen9291@gmail.com>
2022-01-19 12:13:56 -0500

    api call

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import * as ReactBootStrap from 'react-bootstrap'; 
+const Home = () =>{
+    const [ lyricsItem, setLyricsItem ] = useState(null);
+    const [ loading, setLoading ] = useState(null);
 
-function Home(props) {
-    return (
-        <div className="mainBackground">
-           <center>Home Page</center> 
-        </div>
-    );
+    const getLyrics = async () =>{
+        try{
+            const data = await axios.get(`https://api.lyrics.ovh/v1/rihanna/diamonds`)
+            .then(resp =>{
+                console.log(resp);
+                setLyricsItem(resp.data.lyrics);
+            })
+
+        }catch(e){
+            console.log(e)
+        }
+    };
+
+    useEffect(()=> {
+        getLyrics();
+    },[]);
+
+    return(
+        <>
+        <center><div>Home Page</div></center>
+        {<ReactBootStrap.Spinner animation="border" />}
+        <center><h5>{lyricsItem}</h5></center>
+        </>
+    )
 }
-
 export default Home;
